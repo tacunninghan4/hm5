5d36d1730cd8bb64d41c0a92b66ee5aedb239593
Todd <andrewcunningham770@gmail.com>
2021-07-15 08:20:41 -0400

    first commit

---
+++ js index.js
@@ -0,0 +1,68 @@
+var a = 1
+
+var b = 2
+
+var c = 3
+
+var d = 4
+
+var e = 5
+
+var f = 6
+
+var g = 7
+
+var h = 8
+
+var i = 9
+
+var j = 10
+
+console.log (a + b)
+console.log (c + d)
+
+console.log (e - f)
+console.log (g - h)
+
+console.log (i * j)
+console.log (a * b)
+
+console.log (c / d)
+console.log (e / f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
